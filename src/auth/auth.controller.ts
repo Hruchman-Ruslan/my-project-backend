e36ffd2b2cf0e9884b663ec1c0a2e68c9e593642
Ruslan <Giovingo92@gmail.com>
2025-05-21 20:19:26 +0300

    feat: absolute paths

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { CreateUserDto } from '@app/users/dto/createUser.dto';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signUp')
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
+  }
+}
