baad793a6f1384287b2d3b157ba268a9bbddd576
Ruslan <Giovingo92@gmail.com>
2025-05-22 19:48:58 +0300

    fix: cleare cls

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -4,14 +4,6 @@ dotenv.config();
 
 import { DataSource } from 'typeorm';
 
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD ? '***' : 'undefined',
-  DB_NAME: process.env.DB_NAME,
-});
-
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
